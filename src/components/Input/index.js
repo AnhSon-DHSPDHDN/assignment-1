802944aa5f82243ed36d132b6129da5178d380e2
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-06-09 20:11:17 +0700

    ref Feature add task

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react'
 import './style.css'
 
 export default class Input extends Component {
-  constructor() {
-    super()
-    this.handleChangeInputTask = this.handleChangeInputTask.bind(this)
-  }
-
-  handleChangeInputTask(e) {
-    console.log(e.target.value)
+  constructor(props) {
+    super(props)
+    this.props = props
   }
 
   render() {
     return (
       <input
         className='input input--task'
-        placeholder='Add new task in here' onChange={this.handleChangeInputTask}>
-      </input>
+        placeholder='Add new task in here'
+        value={this.props.value}
+        onChange={this.props.handleChangeInputTask}
+      />
     )
   }
 }
